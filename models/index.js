d8a2ae3aedd33d38b0ab51abd999b2f7ab6f32ba
samia <dioua.samia@gmail.com>
2019-06-11 21:37:43 +0200

    second commit

--- models/index.js
+++ models/index.js
@@ -2,11 +2,9 @@ const Event = require("./event");
 const Message= require("./message");
 const Share=require("./share");
 const User=require("./user");
-const Register=require("./register");
 module.exports = {
   Event,
   User,
   Message,
-  Share,
-  Register
+  Share
 };
